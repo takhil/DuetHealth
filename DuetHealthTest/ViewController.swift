59c8480921caf710d21f9c70ba36cab7e5ef6925
Tirumalasetty, Akhil <akhil.tirumalasetty@one.verizon.com>
2016-08-12 10:29:05 -0400

    Initial Commit

---
+++ DuetHealthTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DuetHealthTest
+//
+//  Created by Tirumalasetty, Akhil on 8/12/16.
+//  Copyright © 2016 Verizon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
